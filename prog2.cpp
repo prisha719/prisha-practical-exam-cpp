40319656a9a1ef4c885f8f8b0b72f24408034418
prishaa <prishapuj7@gmail.com>
2025-12-12 17:03:47 +0530

    Add files via upload

--- prog2.cpp
+++ prog2.cpp
@@ -1,69 +1,122 @@
 #include <iostream>
 using namespace std;
 
-class Employee {
+class Employee
+{
 private:
     string name;
-    float salary;
-    string designation;
+    double basicSalary;
+    double hra, da, ta, ma, pf;
 
 public:
-    
+
     void setName(string n)
-     {
+    {
         name = n;
     }
 
-    void setSalary(float s) 
+    void setBasicSalary(double bs)
     {
-        salary = s;
+        basicSalary = bs;
     }
 
-    void setDesignation(string d)
-     {
-        designation = d;
+    void setHRA(double h)
+    {
+        hra = basicSalary * 0.05;
     }
 
-    
-    string getName() 
-    
+    void setDA(double d)
+    {
+        da = basicSalary * 0.1;
+    }
+
+    void setTA(double t)
+    {
+        ta = basicSalary * 0.03;
+    }
+
+    void setMA(double m)
+    {
+        ma = basicSalary * 0.05;
+    }
+
+    void setPF(double p)
+    {
+        p = 200;
+        pf = p;
+    }
+
+    string getName()
     {
         return name;
     }
 
-    float getSalary() {
-        return salary;
+    double getBasicSalary()
+    {
+        return basicSalary;
     }
 
-    string getDesignation() {
-        return designation;
+    double getHRA()
+    {
+        return hra;
+    }
+
+    double getDA()
+    {
+        return da;
+    }
+
+    double getTA()
+    {
+        return ta;
+    }
+
+    double getMA()
+    {
+        return ma;
+    }
+
+    double getPF()
+    {
+        return pf;
+    }
+
+    double getNetSalary()
+    {
+        return (basicSalary + hra + da + ta + ma) - pf;
     }
 };
 
-int main() 
+int main()
 {
     Employee emp;
-    string n, d;
-    float s;
+    string name;
+    double basic, hra, da, ta, ma, pf;
 
-    cout << "Enter your Name: ";
-    getline(cin, n);
-    emp.setName(n);
+    cout << "Enter Employee Name: ";
+    getline(cin, name);
 
-    cout << "Enter Salary: ";
-    cin >> s;
-    emp.setSalary(s);
+    cout << "Enter Basic Salary: ";
+    cin >> basic;
 
-    cin.ignore(); 
+    emp.setName(name);
+    emp.setBasicSalary(basic);
+    emp.setHRA(hra);
+    emp.setDA(da);
+    emp.setTA(ta);
+    emp.setMA(ma);
+    emp.setPF(pf);
 
-    cout << "Enter Designation: ";
-    getline(cin, d);
-    emp.setDesignation(d);
-
-    cout << "\n--- Employee Details ---\n";
+    cout << "\n--- Employee Salary Details ---\n";
     cout << "Name: " << emp.getName() << endl;
-    cout << "Salary: " << emp.getSalary() << endl;
-    cout << "Designation: " << emp.getDesignation() << endl;
+    cout << "Basic Salary: " << emp.getBasicSalary() << endl;
+    cout << "HRA: " << emp.getHRA() << endl;
+    cout << "DA: " << emp.getDA() << endl;
+    cout << "TA: " << emp.getTA() << endl;
+    cout << "MA: " << emp.getMA() << endl;
+    cout << "PF: " << emp.getPF() << endl;
+
+    cout << "Net Salary: " << emp.getNetSalary() << endl;
 
     return 0;
 }
